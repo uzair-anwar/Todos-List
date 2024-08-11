d3e532644dd4fd904f594d9a13cc5a231de34ff4
Uzair Anwar <uzairch688@gmail.com>
2024-08-11 13:56:35 -0500

    Create todos list

---
+++ TodoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Minimal_API
+{
+    class TodoDB: DbContext
+    {
+        public TodoDB(DbContextOptions<TodoDB> options):base(options) { }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
